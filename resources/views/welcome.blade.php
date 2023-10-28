5ef252ccee6f1278d1bc993c5527b519371af375
carlos manuel ozuna ramirez <l20270254@tuxtla.tecnm.mx>
2023-10-27 22:44:40 -0600

    Ozuna Ramirez Cambio otra vez

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -106,10 +106,10 @@
                                     </svg>
                                 </div>
 
-                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Vibrant Ecosystem</h2>
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Carlitos grocery the lowest prices in Nuevo Leon</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                    Existen muchas cosas interesantes con laravel
+                                    Esto fue editado por carlos manuel
 
                                 </p>
                             </div>
